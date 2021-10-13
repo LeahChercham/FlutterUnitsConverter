3111203231679ebe78e93616f48c5e30a51a5936
Leah Chercham <leahchercham@hotmail.com>
2021-10-13 13:56:53 +0200

    done

--- lib/main.dart
+++ lib/main.dart
@@ -26,24 +26,24 @@ class ConverterScreen extends StatefulWidget {
   ConverterScreen({Key? key, required this.title}) : super(key: key);
   final String title;
   @override
-  _ConverterScreenState createState() => _ConverterScreenState();
+  ConverterScreenState createState() => ConverterScreenState();
 }
 
-class _ConverterScreenState extends State<ConverterScreen> {
-  double _startValue = 0;
-  var _startUnit = "Meter";
-  var _endUnit = "Feet";
-  double _endValue = 0;
+class ConverterScreenState extends State<ConverterScreen> {
+  // Definition of standard start values
+  double startValue = 0;
+  var startUnit = "Meter";
+  var endUnit = "Feet";
+  double endValue = 0;
   var textController = TextEditingController();
   bool showResult = false;
   bool blockConversion = false;
-  String _result = "";
+  String result = "";
   List<String> units = ['Meter', 'Kilometer', 'Feet', 'Mile'];
-
   var currentFocus;
 
   void unfocus() {
-    // function to hide nummeric keyboard
+    // Function to hide nummeric keyboard
     currentFocus = FocusScope.of(context);
 
     if (!currentFocus.hasPrimaryFocus) {
@@ -51,219 +51,231 @@ class _ConverterScreenState extends State<ConverterScreen> {
     }
   }
 
-  void _pressButton() {
+  void pressButton() {
+    // Function called after press on convert Button
     unfocus(); // Hide Numeric Keyboard
-    _convertValue();
+    convertValue();
 
     setState(() {
       showResult = true;
     });
   }
 
-  void _convertValue() {
+  void convertValue() {
+    // Conversion function
     if (blockConversion) {
-      return; // do nothing
+      return; // Do nothing
     } else {
-      switch (_startUnit) {
+      // If conversion allowed
+      // Switch statement for conversions. (Better solution: matrix or 2d array)
+      switch (startUnit) {
         case "Meter":
-          switch (_endUnit) {
+          switch (endUnit) {
             case "Feet":
               setState(() {
-                _endValue = _startValue * 3.28084;
+                endValue = startValue * 3.28084;
               });
               break;
             case "Kilometer":
               setState(() {
-                _endValue = _startValue * 0.001;
+                endValue = startValue * 0.001;
               });
               break;
             case "Mile":
               setState(() {
-                _endValue = _startValue * 0.000621371;
+                endValue = startValue * 0.000621371;
               });
               break;
             case "Meter":
               setState(() {
-                _endValue = _startValue * 1;
+                endValue = startValue * 1;
               });
               break;
           }
           break;
 
         case "Kilometer":
-          switch (_endUnit) {
+          switch (endUnit) {
             case "Feet":
               setState(() {
-                _endValue = _startValue * 3280.84;
+                endValue = startValue * 3280.84;
               });
               break;
             case "Kilometer":
               setState(() {
-                _endValue = _startValue * 1;
+                endValue = startValue * 1;
               });
               break;
             case "Mile":
               setState(() {
-                _endValue = _startValue * 0.621371;
+                endValue = startValue * 0.621371;
               });
               break;
             case "Meter":
               setState(() {
-                _endValue = _startValue * 1000;
+                endValue = startValue * 1000;
               });
               break;
           }
           break;
 
         case "Feet":
-          switch (_endUnit) {
+          switch (endUnit) {
             case "Feet":
               setState(() {
-                _endValue = _startValue * 1;
+                endValue = startValue * 1;
               });
               break;
             case "Kilometer":
               setState(() {
-                _endValue = _startValue * 0.0003048;
+                endValue = startValue * 0.0003048;
               });
               break;
             case "Mile":
               setState(() {
-                _endValue = _startValue * 0.000189394;
+                endValue = startValue * 0.000189394;
               });
               break;
             case "Meter":
               setState(() {
-                _endValue = _startValue * 0.3048;
+                endValue = startValue * 0.3048;
               });
               break;
           }
           break;
 
         case "Mile":
-          switch (_endUnit) {
+          switch (endUnit) {
             case "Feet":
               setState(() {
-                _endValue = _startValue * 5280;
+                endValue = startValue * 5280;
               });
               break;
             case "Kilometer":
               setState(() {
-                _endValue = _startValue * 1.60934;
+                endValue = startValue * 1.60934;
               });
               break;
             case "Mile":
               setState(() {
-                _endValue = _startValue * 1;
+                endValue = startValue * 1;
               });
               break;
             case "Meter":
               setState(() {
-                _endValue = _startValue * 1609.34;
+                endValue = startValue * 1609.34;
               });
               break;
           }
           break;
       }
       setState(() {
-        _result = '$_startValue $_startUnit sind $_endValue $_endUnit';
+        result = '$startValue $startUnit sind $endValue $endUnit';
       });
     }
-  }
+  } // End of Switch Statement.
 
   @override
   Widget build(BuildContext context) {
-    // visible content of app
+    // Visible Content of App
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Padding(
-        padding:
-            EdgeInsets.only(left: 20.0, top: 20.0, right: 20.0, bottom: 20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Text(
-              'Wert',
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              controller: textController,
-              decoration: InputDecoration(
-                  border: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue),
-                  ),
-                  labelText: "Hier zu konvertierenden Wert eingeben"),
-              onChanged: (String newValue) {
-                showResult = false;
-                if (double.tryParse(newValue) == null || newValue == "") {
-                  setState(() {
-                    _result = (_startValue == 0 || newValue == "")
-                        ? ""
-                        : "Nur Dezimalzahlen mit Punkt als Trennzeichen sind erlaubt.";
-                    showResult = true;
-                    blockConversion = true;
-                  });
-                } else {
-                  setState(() {
-                    _startValue = double.parse(newValue);
-                    blockConversion = false;
-                  });
-                }
-              },
-            ),
-            Text('Von'),
-            DropdownButton<String>(
-              isExpanded: true,
-              value: _startUnit,
-              icon: const Icon(Icons.arrow_downward),
-              iconSize: 24,
-              elevation: 16,
-              underline: Container(height: 2, color: Colors.blue[900]),
-              onChanged: (String? newValue) {
-                setState(() {
-                  _startUnit = newValue!;
-                  showResult = false;
-                });
-              },
-              items: units.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            Text('Nach'),
-            DropdownButton<String>(
-              isExpanded: true,
-              value: _endUnit,
-              icon: const Icon(Icons.arrow_downward),
-              iconSize: 24,
-              elevation: 16,
-              underline: Container(
-                height: 2,
-                color: Colors.blue[900],
-              ),
-              onChanged: (String? newValue) {
-                setState(() {
-                  _endUnit = newValue!;
-                  showResult = false;
-                });
-              },
-              items: units.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            ElevatedButton(
-                onPressed: _pressButton, child: const Text('Konvertieren')),
-            showResult ? Text('$_result') : Text(""),
-          ],
+        // Top level container
+        // resizeToAvoidBottomInset:false,
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-    );
+        body: Padding(
+          padding:
+              EdgeInsets.only(left: 20.0, top: 20.0, right: 20.0, bottom: 20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                // Start of Widget Array
+                Text(
+                  'Wert',
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  controller: textController,
+                  decoration: InputDecoration(
+                      border: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      labelText: "Hier zu konvertierenden Wert eingeben"),
+                  onChanged: (String newValue) {
+                    showResult = false;
+                    if (double.tryParse(newValue) == null || newValue == "") {
+                      // Error handling
+                      setState(() {
+                        result = (startValue == 0 || newValue == "")
+                            ? ""
+                            : "Nur Dezimalzahlen mit Punkt als Trennzeichen sind erlaubt.";
+                        showResult = true;
+                        blockConversion = true;
+                      });
+                    } else {
+                      // If input correct
+                      setState(() {
+                        startValue = double.parse(newValue);
+                        blockConversion = false;
+                      });
+                    }
+                  },
+                ),
+                Padding(padding:EdgeInsets.fromLTRB(10, 20, 10, 0),
+          child: Text('Von')),
+                DropdownButton<String>(
+                  isExpanded: true,
+                  value: startUnit,
+                  icon: const Icon(Icons.arrow_downward),
+                  iconSize: 24,
+                  elevation: 16,
+                  underline: Container(height: 2, color: Colors.blue[900]),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      startUnit = newValue!;
+                      showResult = false;
+                    });
+                  },
+                  items: units.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                Padding(padding:EdgeInsets.fromLTRB(10, 20, 10, 0),
+          child: Text('Nach')),
+                DropdownButton<String>(
+                  isExpanded: true,
+                  value: endUnit,
+                  icon: const Icon(Icons.arrow_downward),
+                  iconSize: 24,
+                  elevation: 16,
+                  underline: Container(
+                    height: 2,
+                    color: Colors.blue[900],
+                  ),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      endUnit = newValue!;
+                      showResult = false;
+                    });
+                  },
+                  items: units.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                ElevatedButton(
+                    onPressed: pressButton, child: const Text('Konvertieren')),
+                showResult ? Text('$result') : Text(""),
+              ],
+            ),
+          ),
+        ));
   }
 }
